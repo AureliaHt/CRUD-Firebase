dcb82d130dcc01293ee77cf93204c91318e7d8d4
Aurélia Hemeret <aurelia.hemeret@gmail.com>
2022-05-04 17:29:34 +0200

    user's page when connected

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import {useState} from "react";
 import ConnectModal from "./components/ConnectModal";
 import {auth} from "./utils/firebase.config";
 import {onAuthStateChanged} from "firebase/auth";
+import CreatePosts from "./components/CreatePosts";
+import {AiOutlineLogout} from "react-icons/ai";
 
 function App() {
   const [user, setUser] = useState(null);
@@ -13,7 +15,14 @@ function App() {
   return (
     <div>
         <div className="app-header">
-            <ConnectModal />
+          {user && (
+            <div className="user-infos">
+              <span>{user?.displayName[0]}</span> 
+              <h4>{user?.displayName}</h4>
+              <button><AiOutlineLogout/></button>
+            </div>
+          )}
+          {user ? <CreatePosts/> : <ConnectModal /> }
         </div>
         <div className="posts-container"></div>
     </div>
---
+++ client/src/components/CreatePosts.js
@@ -0,0 +1,12 @@
+const CreatePosts = () => {
+    return (
+        <div className="nex-post-modal">
+            <form>
+                <textarea placeholder="Message..."></textarea>
+                <input type="submit" value="Envoyer"/>
+            </form>
+        </div>
+    );
+};
+
+export default CreatePosts;
