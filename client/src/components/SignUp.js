400d7f26811975dc8dcbb4e59518fe68ecb7a114
Aurélia Hemeret <aurelia.hemeret@gmail.com>
2022-05-04 15:37:56 +0200

    environment variable

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,7 +1,15 @@
 const SignUp = () => {
     return (
-        <div>
-            <h2>S'inscrire</h2>
+        <div className="signup-container">
+            <div className="signup">
+                <h3>S'inscrire</h3>
+                <form>
+                    <input type="text" placeholder="identifiant" required/>
+                    <input type="email" placeholder="email" required/>
+                    <input type="password" placeholder="mot de passe" required/>
+                    <input type="submit" value="Valider l'inscription"/>
+                </form>
+            </div>
         </div>
     );
 };
--- client/src/utils/firebase.config.js
+++ client/src/utils/firebase.config.js
@@ -1,13 +1,14 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 
+
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyBfTqRC_bUI4hZwBnXN6fPlJKiOsv_fvfI",
-  authDomain: "crud-firebase-5c80e.firebaseapp.com",
-  projectId: "crud-firebase-5c80e",
-  storageBucket: "crud-firebase-5c80e.appspot.com",
-  messagingSenderId: "513800770384",
-  appId: "1:513800770384:web:76349a53edd056339f1255"
+  apiKey: REACT_APP_SECRET_API_KEY,
+  authDomain: REACT_APP_SECRET_AUTH_DOMAIN,
+  projectId: REACT_APP_SECRET_PROJECT_ID,
+  storageBucket: REACT_APP_SECRET_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_SECRET_MESSAGING_SENDER_ID,
+  appId: REACT_APP_SECRET_APP_ID
 });
 
 export const auth = app.auth();
